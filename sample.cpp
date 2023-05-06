feee425fa99c031a04c18017867828f1da11f8d5
saurabhlanje <40294372+saurabhlanje@users.noreply.github.com>
2023-05-06 19:01:27 +0530

    Update sample.cpp

--- sample.cpp
+++ sample.cpp
@@ -10,7 +10,9 @@ using namespace Eigen;
 
 void step_next()
 {
-    cout << "____________________________________________________________" << endl<< endl << endl;
+    cout << "____________________________________________________________" << endl
+         << endl
+         << endl;
 }
 int main(int argc, char **argv)
 {
@@ -51,59 +53,122 @@ int main(int argc, char **argv)
         cout << x << " ";
     cout << "\n";
 
-
     step_next();
-    //Iterating over rows or columns of 2D arrays and matrices
+    // Iterating over rows or columns of 2D arrays and matrices
 
-    ArrayXXf B = ArrayXXf::Random(4,4).abs();
-    cout << "Here is the initial matrix B:\n" << B << "\n";
-    for(auto row : B.rowwise())
-      std::sort(row.begin(), row.end());
-    cout << "Here is the sorted matrix B:\n" << B << "\n";
+    ArrayXXf B = ArrayXXf::Random(4, 4).abs();
+    cout << "Here is the initial matrix B:\n"
+         << B << "\n";
+    for (auto row : B.rowwise())
+        std::sort(row.begin(), row.end());
+    cout << "Here is the sorted matrix B:\n"
+         << B << "\n";
 
+    // Define matrix with varying size during runtume
+    Matrix<double, Dynamic, Dynamic> matt; // size is not defined and memory is not allocated and matrix is not initialized
+    // OR
+    MatrixXd matri(10, 10); // here memory is allocated but matrix is not initiated
 
-    //Define matrix with varying size during runtume
-    Matrix<double, Dynamic, Dynamic> matt; //size is not defined and memory is not allocated and matrix is not initialized
-    //OR
-    MatrixXd matri(10,10);// here memory is allocated but matrix is not initiated
-
-    //Matlab counts from 1 but Eigen started from 0
+    // Matlab counts from 1 but Eigen started from 0
 
     // Matrix an be added like this as well
     step_next();
-    cout<<"Different method to add value to matrix"<<endl;
+    cout << "Different method to add value to matrix" << endl;
     Matrix4f matr;
-    matr<<1,2,3,4,
-    5,6,7,8,
-    9,10,11,12,
-    13,14,15,16;
-    cout<<matr<<endl;
+    matr << 1, 2, 3, 4,
+        5, 6, 7, 8,
+        9, 10, 11, 12,
+        13, 14, 15, 16;
+    cout << matr << endl;
 
-    //Dynamically define matrix of zero/ones/constant
+    // Dynamically define matrix of zero/ones/constant
     step_next();
-    cout<<"different to make zero in all the places within matrix";
-    int row=5;
-    int col=5;
+    cout << "different to make zero in all the places within matrix";
+    int row = 5;
+    int col = 5;
     MatrixXf matrix1zeros;
-    matrix1zeros=MatrixXf::Zero(row,col);
-    cout<<endl<<matrix1zeros<<endl;
-    //Same thing can be done in this way as well
-    
+    matrix1zeros = MatrixXf::Zero(row, col);
+    cout << endl
+         << matrix1zeros << endl;
+    // Same thing can be done in this way as well
+
     MatrixXf matrix2zeros;
-    matrix2zeros.setZero(row,col);
-    cout<<endl<<matrix2zeros<<endl;
+    matrix2zeros.setZero(row, col);
+    cout << endl
+         << matrix2zeros << endl;
 
-    //other methods like ones,constants rand random, Identity also works
-    
+    // other methods like ones,constants rand random, Identity also works
 
+    // Access matrix blocks
+    //.block(a,b,c,d)
+    // a,b is start position
+    // c,d, are the height and width of block
 
+    MatrixXd matrixv(4, 4);
+    matrixv << 101, 102, 103, 104,
+        105, 106, 107, 108,
+        109, 110, 111, 112,
+        113, 114, 115, 116;
+    cout << "Initial matrix is " << endl;
 
+    cout << matrixv << endl;
+    MatrixXd blocked_mat = matrixv.block(1, 0, 3, 2); // start position row, start pos column, height, width
+    cout << "block of matrix is" << endl;
+    cout << endl
+         << blocked_mat << endl;
 
+    // Access individual rows and columns of matrix
+    cout << "row 0 of above matrix is " << endl;
+    cout << matrixv.row(0) << endl;
+    cout << endl
+         << "col 0 of above matrix is " << endl;
+    cout << matrixv.col(0) << endl;
 
+    // Create diagonal matrix
+    Matrix<double, 3, 1> vector1;
+    vector1 << 1, 2, 3;
+    cout << endl
+         << "Initial matrix is" << endl
+         << vector1;
+    MatrixXd diagonalmatrix;
+    diagonalmatrix = vector1.asDiagonal();
+    cout << endl
+         << "diagonal matrix is" << endl
+         << diagonalmatrix << endl;
 
+    // Basic matrix operations
+    step_next();
+    MatrixXd A1(2, 2);
+    MatrixXd B1(2, 2);
 
+    A1 << 1, 2, 3, 4;
+    B1 << 3, 4, 5, 6;
+    cout << endl
+         << A1 << endl;
+    cout << endl
+         << B1 << endl;
 
+    MatrixXd C1 = A1 + B1; // Addition
+    cout << "Matrix addition is " << endl
+         << C1 << endl;
+    MatrixXd D1 = A1 * B1; // Multiplication
+    cout << "Matrix multiplication is " << endl
+         << D1 << endl;
 
+    // Multiplication by scalar
+    int s = 2;
+    MatrixXd F1;
+    F1 = s * A1;
+    cout << endl <<"Matrix multiplication by scalar is"<< endl<<F1 << endl;
 
+    //Matrix transpose
+    MatrixXd At;
+    MatrixXd R1;
+    At=A1.transpose();
+    cout<<endl<<"Transpose of A1 is"<<endl<<At<<endl;
+
+    //Matrix transpose in place
+    A1.transposeInPlace();//this is specific method just to get the transpose at the place itself A1=At.transpose() will not work at all
+    cout<<endl<<"In place transpose of A1"<<endl<<A1<<endl;
 
 }
